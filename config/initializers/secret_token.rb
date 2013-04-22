ae7b006389cc1b096b0f4bb0164b40560a1727ae
Alex Clemmer <clemmer.alexander@gmail.com>
2013-04-22 14:40:04 -0600

    Initial version of the regret site

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Regret::Application.config.secret_token = '315c994b747b2b42cf0418b8a366ef84f3728461f8c1e16c069aa5082ea9b19dc20325c60f64c2bd21d29126766f36470b4fecd3e0619e965ae0bb6a81e87c0c'
